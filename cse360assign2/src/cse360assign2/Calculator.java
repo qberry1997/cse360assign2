e9fca141008511e0f41cb626a7d6ae161a020278
quinn_000 <quinn_000@Quinn_Computer>
2019-02-24 23:14:51 -0700

    Initial Version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,73 @@
+package cse360assign2;
+/**
+ * The Calculator class will compute mathematical operators applied
+ * to given operands. This class will also be able to report a
+ * history of previously executed operations.
+ * 
+ * @author quinn
+ * @version 1.0
+ * @since 2/23/2019
+ */
+
+public class Calculator {
+	/**
+	 * The total computed by the calculator
+	 */
+	private int total;
+	
+	/**
+	 * Constructor for a calculator object.
+	 * Initialize the total to 0.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Retrieve the current total of the calculator object.
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method will add a given value to the total.
+	 * @param value The value to be added to the current total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method will subtract a given value from the total.
+	 * @param value The value to be subtracted from the current total.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method will multiply a value by the total.
+	 * @param value The value to be multiplied by the total.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method will be divide the total by the given value.
+	 * @param value The value that will divide the current total.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Retrieve the current total of the calculator object.
+	 * @return The current total of the calculator object.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
